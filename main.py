cbba31c8833fdb08f1d5b4c9458ef52467ddd167
Adrian Manhey <adrianmanhey@gmail.com>
2019-07-25 17:39:41 -0700

    Changed the nickname filling of the welcome page

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ class WelcomePage(webapp2.RequestHandler):
         self.response.write(welcome_template.render())
     def post(self):
         welcome_template = JINJA_ENVIRONMENT.get_template('welcome_back.html')
-        nameValue = self.request.get('Nickname')
+        user = users.get_current_user()
+        nameValue = user.nickname
         self.response.write(welcome_template.render(welcome_dict(nameValue)))
         #update the nickname in the datastore
         user_profile(self,0,'Nickname',nickname)
